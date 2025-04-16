2d198cb102f15d4d6e65b003e8c6bee199608e8c
hoppeta20230731 <sra6road6mt@gmail.com>
2025-04-16 15:54:13 +0900

    ステータス管理カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_11_025600) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_16_065213) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_11_025600) do
     t.string "slack_message_ts"
     t.text "slack_replies_data"
     t.text "ai_feedback"
+    t.string "ai_feedback_status"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
