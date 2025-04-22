72b86889383fe6d5722eddeaee9d3b8932556ddb
hoppeta20230731 <sra6road6mt@gmail.com>
2025-04-23 08:28:06 +0900

    Basic認証導入
    本番環境のみ適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,17 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+
+  def basic_auth
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        # credentials から値を取得
+        username == Rails.application.credentials.dig(:basic_auth, :username) &&
+        password == Rails.application.credentials.dig(:basic_auth, :password)
+      end
+    end
   end
 end
